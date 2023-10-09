907e8a23eb34609c82027502411340c6796bc0a9
Victor Izuchukwu Ihemadu <bigDev@Victors-MacBook-Air.local>
2023-10-09 17:55:33 +0100

    saveToFile

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"github.com/bigDev/cardsGame/deck"
 )
 
 
+
+
 func main() {
 	cards := deck.NewDeck()
+
+	fmt.Println(cards.ToString())
 	
-	cards.Print()
+	// cards.Print()
+	
+	// hand, remainingCards := deck.Deal(cards, 5)
+
+	// hand.Print()
+	// remainingCards.Print()
+
+
 }
