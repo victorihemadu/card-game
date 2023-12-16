b88d761b69c909b07627ea5b958501bf22cfe8f2
Victor Izuchukwu Ihemadu <bigDev@Victors-MacBook-Air.local>
2023-12-16 02:51:05 +0100

    New Test FUnction

--- deck/deck_test.go
+++ deck/deck_test.go
@@ -1,1 +1,37 @@
 package deck
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := NewDeck()
+
+	if len(d) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card to be Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card to be Four of Clubs but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	d := NewDeck()
+	d.SaveToFile("_decktesting")
+
+	loadedDeck := NewDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+}
